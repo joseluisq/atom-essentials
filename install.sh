923bdafd07cac8f0d751c4ae86f3d848043f17df
José Quintana <joseluisquintana20@gmail.com>
2016-02-27 11:07:36 -0500

    Add icon-fonts atom-bootstrap4 language-fish-shell

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apm install editorconfig linter linter-xo linter-sass-lint docblockr atom-ternjs esformatter autocomplete-paths pigments emmet file-icons minimap
+apm install editorconfig linter linter-xo linter-sass-lint docblockr atom-ternjs esformatter autocomplete-paths pigments emmet file-icons minimap icon-fonts atom-bootstrap4 language-fish-shell
