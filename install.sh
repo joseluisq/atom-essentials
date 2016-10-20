c93405fda10c9e0713e7f4b762c5db3bb195df43
José Quintana <joseluisquintana20@gmail.com>
2016-10-19 21:01:34 -0500

    Full atom package list

--- install.sh
+++ install.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-apm install editorconfig linter linter-xo linter-sass-lint docblockr atom-ternjs esformatter autocomplete-paths pigments emmet file-icons minimap icon-fonts atom-bootstrap4 language-fish-shell
+apm install \
+atom-handlebars \
+atom-ternjs \
+autocomplete-go \
+autocomplete-paths \
+autocomplete-php \
+builder-go \
+docblockr \
+editorconfig \
+emmet \
+environment \
+file-icons \
+go-config \
+go-debug \
+go-get \
+go-plus \
+godoc \
+gofmt \
+gometalinter-linter \
+gorename \
+language-babel \
+language-fish-shell \
+language-varnish \
+linter \
+linter-js-standard \
+linter-php \
+linter-sass-lint \
+minimap \
+navigator-go \
+php-cs-fixer \
+pigments \
+standard-formatter \
+tester-go
